666b1685049e39e37d3355be7fe43e89d0d78d54
James Munnelly <james.munnelly@jetstack.io>
2018-08-07 16:26:32 +0100

    Refactor ACME Issuer to create and manage Order resources

--- pkg/issuer/acme/issue.go
+++ pkg/issuer/acme/issue.go
@@ -8,13 +8,12 @@ import (
 
 	"github.com/golang/glog"
 	corev1 "k8s.io/api/core/v1"
-	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	"github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha1"
 	"github.com/jetstack/cert-manager/pkg/util/errors"
 	"github.com/jetstack/cert-manager/pkg/util/kube"
 	"github.com/jetstack/cert-manager/pkg/util/pki"
-	acmeapi "github.com/jetstack/cert-manager/third_party/crypto/acme"
 )
 
 const (
@@ -27,42 +26,34 @@ const (
 )
 
 func (a *Acme) obtainCertificate(ctx context.Context, crt *v1alpha1.Certificate) ([]byte, []byte, error) {
-	commonName := pki.CommonNameForCertificate(crt)
-	altNames := pki.DNSNamesForCertificate(crt)
+	if crt.Status.ACMEStatus().OrderRef == nil || crt.Status.ACMEStatus().OrderRef.Name == "" {
+		return nil, nil, fmt.Errorf("status.acme.orderRef.name must be set")
+	}
 
-	cl, err := a.helper.ClientForIssuer(a.issuer)
+	orderName := crt.Status.ACMEStatus().OrderRef.Name
+	order, err := a.orderLister.Orders(crt.Namespace).Get(orderName)
 	if err != nil {
-		crt.UpdateStatusCondition(v1alpha1.CertificateConditionReady, v1alpha1.ConditionFalse, errorIssueError, fmt.Sprintf("Failed to get ACME client: %v", err), false)
-		return nil, nil, fmt.Errorf("error creating ACME client: %s", err.Error())
-	}
-
-	orderURL := crt.Status.ACMEStatus().Order.URL
-	if orderURL == "" {
-		crt.UpdateStatusCondition(v1alpha1.CertificateConditionReady, v1alpha1.ConditionFalse, errorInvalidConfig, "status.acme.order.url must be set", false)
-		return nil, nil, fmt.Errorf("certificate order url cannot be blank")
-	}
-
-	order, err := cl.GetOrder(ctx, orderURL)
-	if err != nil {
-		crt.UpdateStatusCondition(v1alpha1.CertificateConditionReady, v1alpha1.ConditionFalse, errorIssueError, fmt.Sprintf("Failed to get order details: %v", err), false)
-		return nil, nil, fmt.Errorf("error getting order details: %v", err)
-	}
-
-	if order.Status != acmeapi.StatusReady {
-		err := fmt.Errorf("expected certificate status to be %q, but it is %q", acmeapi.StatusReady, order.Status)
-		// print a more helpful message to users when an order is marked 'valid'.
-		// this happens when all challenges have been completed successfully, but
-		// the acme server has not finished processing the order.
-		if order.Status == acmeapi.StatusValid {
-			err = fmt.Errorf("%v. Waiting until Order transitions into %q state", err, acmeapi.StatusReady)
-		}
-		crt.UpdateStatusCondition(v1alpha1.CertificateConditionReady, v1alpha1.ConditionFalse, errorIssueError, err.Error(), false)
+		// we return err without checking for IsNotFound because Prepare already
+		// performs cleanup in the event the referenced Order does not exist.
+		// this saves us re-implementing missing order handling here.
 		return nil, nil, err
 	}
 
+	// TODO: ensure the names on the Order match the desired names for this Certificate
+	// If not, we should return an error here in order to trigger the hash-detection
+	// logic in Prepare to run.
+
+	cl, err := a.helper.ClientForIssuer(a.issuer)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	commonName := pki.CommonNameForCertificate(crt)
+	altNames := pki.DNSNamesForCertificate(crt)
+
 	// get existing certificate private key
 	key, err := kube.SecretTLSKey(a.secretsLister, crt.Namespace, crt.Spec.SecretName)
-	if k8sErrors.IsNotFound(err) || errors.IsInvalidData(err) {
+	if apierrors.IsNotFound(err) || errors.IsInvalidData(err) {
 		key, err = pki.GeneratePrivateKeyForCertificate(crt)
 		if err != nil {
 			crt.UpdateStatusCondition(v1alpha1.CertificateConditionReady, v1alpha1.ConditionFalse, errorIssueError, fmt.Sprintf("Failed to generate certificate private key: %v", err), false)
@@ -88,21 +79,8 @@ func (a *Acme) obtainCertificate(ctx context.Context, crt *v1alpha1.Certificate)
 	}
 
 	// obtain a certificate from the acme server
-	certSlice, err := cl.FinalizeOrder(ctx, order.FinalizeURL, derBytes)
+	certSlice, err := cl.FinalizeOrder(ctx, order.Status.FinalizeURL, derBytes)
 	if err != nil {
-		// this handles an edge case where a certificate ends out with an order
-		// that is in an invalid state.
-		// ideally we would instead call GetCertificate on the ACME client
-		// instead of FinalizeOrder, which would save us creating a new order
-		// just to issue a new certificate.
-		// The underlying ACME client doesn't expose this though yet.
-		if acmeErr, ok := err.(*acmeapi.Error); ok {
-			if acmeErr.StatusCode >= 400 && acmeErr.StatusCode <= 499 {
-				crt.Status.ACMEStatus().Order.URL = ""
-			}
-		}
-		// TODO: should we also set the FailedValidation status
-		// condition here so back off can be applied?
 		crt.UpdateStatusCondition(v1alpha1.CertificateConditionReady, v1alpha1.ConditionFalse, errorIssueError, fmt.Sprintf("Failed to finalize order: %v", err), false)
 		a.Recorder.Eventf(crt, corev1.EventTypeWarning, errorIssueError, "Failed to finalize order: %v", err)
 		return nil, nil, fmt.Errorf("error getting certificate from acme server: %s", err)
@@ -116,7 +94,7 @@ func (a *Acme) obtainCertificate(ctx context.Context, crt *v1alpha1.Certificate)
 
 	a.Recorder.Eventf(crt, corev1.EventTypeNormal, successCertObtained, "Obtained certificate from ACME server")
 
-	glog.Infof("successfully obtained certificate: cn=%q altNames=%+v url=%q", commonName, altNames, orderURL)
+	glog.Infof("successfully obtained certificate: cn=%q altNames=%+v url=%q", commonName, altNames, order.Status.URL)
 	// encode the private key and return
 	keyPem, err := pki.EncodePrivateKey(key)
 	if err != nil {
