786213fdb1ec45754b358188f4c1116c443b526b
James Munnelly <james@munnelly.eu>
2020-01-23 12:43:28 +0000

    Add release-tars targets to construct cert-manager release artifacts
    Signed-off-by: James Munnelly <james@munnelly.eu>

--- hack/build/print-workspace-status.sh
+++ hack/build/print-workspace-status.sh
@@ -20,6 +20,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../" > /dev/null && pwd )"
+
+source "${SCRIPT_ROOT}/version.sh"
+kube::version::get_version_vars
+
 # AppVersion is set as the AppVersion to be compiled into the controller binary.
 # It's used as the default version of the 'acmesolver' image to use for ACME
 # challenge requests, and any other future provider that requires additional
@@ -35,9 +41,20 @@ fi
 
 # TODO: properly configure this file
 cat <<EOF
-STABLE_DOCKER_REPO ${DOCKER_REPO:-quay.io/jetstack}
-STABLE_DOCKER_TAG ${DOCKER_TAG:-$APP_VERSION}
-STABLE_APP_GIT_COMMIT ${APP_GIT_COMMIT}
-STABLE_APP_GIT_STATE ${GIT_STATE}
-STABLE_APP_VERSION ${APP_VERSION}
+STABLE_BUILD_GIT_COMMIT ${KUBE_GIT_COMMIT-}
+STABLE_BUILD_SCM_STATUS ${KUBE_GIT_TREE_STATE-}
+STABLE_BUILD_SCM_REVISION ${KUBE_GIT_VERSION-}
+STABLE_BUILD_MAJOR_VERSION ${KUBE_GIT_MAJOR-}
+STABLE_BUILD_MINOR_VERSION ${KUBE_GIT_MINOR-}
+STABLE_DOCKER_TAG ${KUBE_GIT_VERSION/+/-}
+STABLE_DOCKER_REGISTRY ${KUBE_DOCKER_REGISTRY:-quay.io/jetstack}
+STABLE_DOCKER_PUSH_REGISTRY ${KUBE_DOCKER_PUSH_REGISTRY:-${KUBE_DOCKER_REGISTRY:-quay.io/jetstack-staging}}
+gitCommit ${KUBE_GIT_COMMIT-}
+gitTreeState ${KUBE_GIT_TREE_STATE-}
+gitVersion ${KUBE_GIT_VERSION-}
+gitMajor ${KUBE_GIT_MAJOR-}
+gitMinor ${KUBE_GIT_MINOR-}
+buildDate $(date \
+  ${SOURCE_DATE_EPOCH:+"--date=@${SOURCE_DATE_EPOCH}"} \
+ -u +'%Y-%m-%dT%H:%M:%SZ')
 EOF
